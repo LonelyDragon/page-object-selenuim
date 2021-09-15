e36ba1c9ede576ad7f845d8e748af141bd493193
Anton Zavodchikov <diagnozzz123@gmail.com>
2021-09-15 18:31:51 +0300

    add sol from 4.2.9

--- test_login_page.py
+++ test_login_page.py
@@ -1,4 +1,5 @@
 from pages.login_page import LoginPage
+from pages.main_page import MainPage
 
 
 url = "http://selenium1py.pythonanywhere.com/ru/accounts/login/"
@@ -22,3 +23,11 @@ def test_login_func_element_exists(browser):
     page = LoginPage(browser, url)
     page.open()
     page.should_be_login_page
+
+def test_guest_can_go_to_login_page(browser):
+    link = "http://selenium1py.pythonanywhere.com"
+    page = MainPage(browser, link)
+    page.open()
+    page.go_to_login_page()
+    login_page = LoginPage(browser, browser.current_url)
+    login_page.should_be_login_page()
