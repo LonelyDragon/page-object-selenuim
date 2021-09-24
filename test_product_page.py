23e5ca3cd696d258fe3c4c5c42ad3bf37cf3757c
Anton Zavodchikov <diagnozzz123@gmail.com>
2021-09-24 16:30:50 +0300

    add sol from 4.3.8

--- test_product_page.py
+++ test_product_page.py
@@ -25,8 +25,8 @@ def test_guest_cant_see_success_message_after_adding_product_to_basket(browser):
     Добавляем товар в корзину 
     Проверяем, что нет сообщения об успехе с помощью is_not_element_present
     '''
-    link = "http://selenium1py.pythonanywhere.com/catalogue/coders-at-work_207/"
-    page = ProductPage(browser, link)
+    url = "http://selenium1py.pythonanywhere.com/catalogue/coders-at-work_207/"
+    page = ProductPage(browser, url)
     page.open()
     page.add_to_card()
     page.should_not_be_success_message()
@@ -37,8 +37,8 @@ def test_guest_cant_see_success_message(browser):
     Проверяем, что нет сообщения об успехе с помощью is_not_element_present
 
     '''
-    link = "http://selenium1py.pythonanywhere.com/catalogue/coders-at-work_207/"
-    page = ProductPage(browser, link)
+    url = "http://selenium1py.pythonanywhere.com/catalogue/coders-at-work_207/"
+    page = ProductPage(browser, url)
     page.open()
     page.should_not_be_success_message()
 
@@ -48,8 +48,21 @@ def test_message_disappeared_after_adding_product_to_basket(browser):
     Добавляем товар в корзину
     Проверяем, что нет сообщения об успехе с помощью is_disappeared
     '''
-    link = "http://selenium1py.pythonanywhere.com/catalogue/coders-at-work_207/"
-    page = ProductPage(browser, link)
+    url = "http://selenium1py.pythonanywhere.com/catalogue/coders-at-work_207/"
+    page = ProductPage(browser, url)
     page.open()
     page.add_to_card()
     page.element_should_disappear()
+
+def test_guest_should_see_login_url_on_product_page(browser):
+    url = "http://selenium1py.pythonanywhere.com/en-gb/catalogue/the-city-and-the-stars_95/"
+    page = ProductPage(browser, url)
+    page.open()
+    page.should_be_login_link()
+
+def test_guest_can_go_to_login_page_from_product_page(browser):
+    url = "http://selenium1py.pythonanywhere.com/catalogue/coders-at-work_207/"
+    page = ProductPage(browser, url)
+    page.open()
+    page.go_to_login_page()
+    page.should_be_login_link()
